68a80002243c371d1dd7114ee00230b45076abc4
Allan Hans <allan-boop>
2024-01-19 19:35:16 +0100

    mlx42 testing

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: ahans <ahans@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:53:38 by ahans             #+#    #+#             */
-/*   Updated: 2024/01/19 15:45:57 by ahans            ###   ########.fr       */
+/*   Updated: 2024/01/19 17:56:42 by ahans            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define WIDTH 512
-#define HEIGHT 512
+#define WIDTH 256
+#define HEIGHT 256
 
-int main()
+static void ft_error(void)
 {
-	char *title = "42";
-	int i;
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
 
-	i = ft_atoi(title);
-	ft_printf("i = %d\n", i);
-	return (0);
+
+int32_t	main(void)
+{
+	// MLX allows you to define its core behaviour before startup.
+	mlx_set_setting(MLX_MAXIMIZED, true);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	if (!mlx)
+		ft_error();
+
+	/* Do stuff */
+
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	ft_memset(img->pixels, 127, img->width * img->height * 4);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+	// mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
